764506177b9bba0e8318c004f79ad09af45c165f
Mike Heath <elcapo@gmail.com>
2013-09-05 15:41:04 -0600

    Added support for setting a .pid file on initialization.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,16 @@
+import cf.component.util.PidFile;
 import cf.spring.YamlPropertyContextInitializer;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import java.io.IOException;
 
 /**
  * @author Mike Heath <elcapo@gmail.com>
@@ -14,10 +19,15 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 public class Main {
 
+	@Bean
+	PidFile pidFile(Environment environment) throws IOException {
+		return new PidFile(environment.getProperty("pidfile"));
+	}
+
 	public static void main(String[] args) {
 		final SpringApplication springApplication = new SpringApplication(Main.class);
 		springApplication.addInitializers(new YamlPropertyContextInitializer("config", "config", "file:config/service-broker.yml"));
-		final ApplicationContext applicationContext = springApplication.run(args);
+		final ConfigurableApplicationContext applicationContext = (ConfigurableApplicationContext) springApplication.run(args);
 
 		final LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
 		final Level level = Level.toLevel(applicationContext.getEnvironment().getProperty("logging.level"), Level.INFO);
