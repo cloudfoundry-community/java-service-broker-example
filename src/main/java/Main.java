5d102d2dfab60f8f5d3b69fa4779f1e4297f2044
Mike Heath <elcapo@gmail.com>
2013-09-05 15:59:41 -0600

    Added NATS support.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,12 @@ import cf.component.util.PidFile;
 import cf.spring.YamlPropertyContextInitializer;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
+import nats.client.Nats;
+import nats.client.spring.NatsBuilder;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,6 +25,11 @@ public class Main {
 	@Bean
 	PidFile pidFile(Environment environment) throws IOException {
 		return new PidFile(environment.getProperty("pidfile"));
+	}
+
+	@Bean
+	Nats nats(ApplicationEventPublisher publisher, Environment environment) {
+		return new NatsBuilder(publisher).addHost(environment.getProperty("nats.url", "nats://localhost")).connect();
 	}
 
 	public static void main(String[] args) {
