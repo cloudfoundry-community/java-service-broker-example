48943a8db0ddd85a8f0836d670a92eebeb7701f8
Mike Heath <elcapo@gmail.com>
2013-09-06 10:03:09 -0600

    Added client token provider config. Cleaned up config in main.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,24 @@
+import cf.client.CloudController;
+import cf.client.DefaultCloudController;
+import cf.client.TokenProvider;
 import cf.component.util.PidFile;
 import cf.nats.CfNats;
 import cf.nats.DefaultCfNats;
 import cf.nats.RouterRegisterHandler;
+import cf.spring.ClientTokenProviderFactoryBean;
+import cf.spring.HttpClientFactoryBean;
 import cf.spring.YamlPropertyContextInitializer;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
 import nats.client.Nats;
 import nats.client.spring.NatsBuilder;
+import org.apache.http.client.HttpClient;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.FactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -47,13 +54,34 @@ public class Main {
 				environment.getProperty("host.local", "127.0.0.1"),
 				Integer.valueOf(environment.getProperty("host.port", "8080")),
 				environment.getProperty("host.public", "service-broker")
-				);
+		);
+	}
+
+	@Bean
+	FactoryBean<HttpClient> httpClient() {
+		return new HttpClientFactoryBean();
+	}
+
+	@Bean
+	CloudController cloudController(HttpClient client, Environment environment) {
+		return new DefaultCloudController(client, environment.getProperty("cloud_controller_url"));
+	}
+
+	@Bean
+	FactoryBean<TokenProvider> tokenProvider(CloudController cloudController, Environment environment) {
+		return new ClientTokenProviderFactoryBean(
+				cloudController,
+				environment.getProperty("client.name"),
+				environment.getProperty("client.secret"));
 	}
 
 	public static void main(String[] args) {
 		final SpringApplication springApplication = new SpringApplication(Main.class);
-		springApplication.addInitializers(new YamlPropertyContextInitializer("config", "config", "file:config/service-broker.yml"));
-		final ConfigurableApplicationContext applicationContext = (ConfigurableApplicationContext) springApplication.run(args);
+		springApplication.addInitializers(new YamlPropertyContextInitializer(
+				"config",
+				"config",
+				"service-broker.yml"));
+		final ApplicationContext applicationContext = springApplication.run(args);
 
 		final LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
 		final Level level = Level.toLevel(applicationContext.getEnvironment().getProperty("logging.level"), Level.INFO);
