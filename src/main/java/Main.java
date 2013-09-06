0162b4fa42ac733104920dfbf7340c18567637aa
Mike Heath <elcapo@gmail.com>
2013-09-06 09:38:05 -0600

    Added support for CF specific NATS.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
 import cf.component.util.PidFile;
+import cf.nats.CfNats;
+import cf.nats.DefaultCfNats;
+import cf.nats.RouterRegisterHandler;
 import cf.spring.YamlPropertyContextInitializer;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
@@ -30,6 +33,21 @@ public class Main {
 	@Bean
 	Nats nats(ApplicationEventPublisher publisher, Environment environment) {
 		return new NatsBuilder(publisher).addHost(environment.getProperty("nats.url", "nats://localhost")).connect();
+	}
+
+	@Bean
+	CfNats cfNats(Nats nats) {
+		return new DefaultCfNats(nats);
+	}
+
+	@Bean
+	RouterRegisterHandler routerRegisterHandler(CfNats cfNats, Environment environment) {
+		return new RouterRegisterHandler(
+				cfNats,
+				environment.getProperty("host.local", "127.0.0.1"),
+				Integer.valueOf(environment.getProperty("host.port", "8080")),
+				environment.getProperty("host.public", "service-broker")
+				);
 	}
 
 	public static void main(String[] args) {
